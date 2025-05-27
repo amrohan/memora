c0f67055c48ba86ef01b13f3093b434d5a4352b5
amrohan <rohanxsalunkhe@gmail.com>
2025-05-27 20:19:34 +0530

    settings page updated

--- src/app/components/collection-card/collection-card.component.ts
+++ src/app/components/collection-card/collection-card.component.ts
@@ -40,7 +40,7 @@ import { Collection } from '@models/collection.model';
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
-              class="size-6 text-primary relative"
+              class="size-6 relative"
             >
               <path
                 d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z"
--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -11,184 +11,200 @@ import { ApiResponse } from '@models/ApiResponse';
 import { AuthService } from '@services/auth.service';
 import { AuthStateService } from '@services/auth-state.service';
 import { ToastService } from '@services/toast.service';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, HeaderComponent],
   template: `
     <section class="mb-26">
-      <div class="flex w-full flex-col">
-        <div class="card rounded-box max-w-5xl">
-          <section>
-            <div class="card bg-base-100">
-              <div class="card-body">
-                <!-- User Information -->
-                <fieldset class="fieldset">
-                  <legend class="fieldset-legend text-base md:text-lg">
-                    User Information
-                  </legend>
-                  <form
-                    [formGroup]="settingsForm"
-                    (ngSubmit)="updateUserInfo()"
-                  >
-                    <div class="form-control w-full mb-4">
-                      <label class="label">
-                        <span class="label-text">Full Name</span>
-                      </label>
-                      <input
-                        type="text"
-                        placeholder="Enter your full name"
-                        class="input input-bordered w-full"
-                        formControlName="name"
-                      />
-                      @if (settingsForm.get('name')?.invalid &&
-                      settingsForm.get('name')?.touched) {
-                      <label class="label">
-                        <span class="label-text-alt text-error"
-                          >Please enter a valid name</span
-                        >
-                      </label>
-                      }
-                    </div>
-
-                    <div class="form-control w-full mb-6">
-                      <label class="label">
-                        <span class="label-text">Email Address</span>
-                      </label>
-                      <input
-                        type="email"
-                        placeholder="Enter your email address"
-                        class="input input-bordered w-full"
-                        formControlName="email"
-                      />
-                      @if (settingsForm.get('email')?.invalid &&
-                      settingsForm.get('email')?.touched) {
-                      <label class="label">
-                        <span class="label-text-alt text-error"
-                          >Please enter a valid email address</span
-                        >
-                      </label>
-                      } @if (fieldErrors()?.['email']) {
-                      <label class="label">
-                        <span
-                          class="label-text-alt text-error"
-                          >{{ fieldErrors()?.['email'] }}</span
-                        >
-                      </label>
-                      }
-                    </div>
-
-                    <div class="flex justify-end">
-                      <button
-                        type="submit"
-                        class="btn btn-primary"
-                        [disabled]="settingsForm.invalid || isLoading()"
-                      >
-                        @if (isLoading()) {
-                        <span class="loading loading-spinner loading-sm"></span>
-                        } Save Changes
-                      </button>
-                    </div>
-                  </form>
-                </fieldset>
-
-                <div class="divider"></div>
-
-                <!-- Change Password -->
-                <fieldset class="fieldset">
-                  <legend class="fieldset-legend text-base md:text-lg">
-                    Change Password
-                  </legend>
-                  <form
-                    [formGroup]="settingsForm"
-                    (ngSubmit)="changePassword()"
-                  >
-                    <div class="form-control w-full mb-4">
-                      <label class="label">
-                        <span class="label-text">Current Password</span>
-                      </label>
-                      <input
-                        type="password"
-                        placeholder="Enter your current password"
-                        class="input input-bordered w-full"
-                        formControlName="currentPassword"
-                      />
-                      @if (settingsForm.get('currentPassword')?.invalid &&
-                      settingsForm.get('currentPassword')?.touched) {
-                      <label class="label">
-                        <span class="label-text-alt text-error"
-                          >Please enter your current password</span
-                        >
-                      </label>
-                      }
-                    </div>
-
-                    <div class="form-control w-full mb-4">
-                      <label class="label">
-                        <span class="label-text">New Password</span>
-                      </label>
-                      <input
-                        type="password"
-                        placeholder="Enter a new password"
-                        class="input input-bordered w-full"
-                        formControlName="newPassword"
-                      />
-                      @if (settingsForm.get('newPassword')?.invalid &&
-                      settingsForm.get('newPassword')?.touched) {
-                      <label class="label">
-                        <span class="label-text-alt text-error"
-                          >Please enter a new password</span
-                        >
-                      </label>
-                      }
-                    </div>
-
-                    <div class="form-control w-full mb-6">
-                      <label class="label">
-                        <span class="label-text">Confirm New Password</span>
-                      </label>
-                      <input
-                        type="password"
-                        class="input input-bordered w-full"
-                        placeholder="Confirm your new password"
-                        formControlName="confirmNewPassword"
-                      />
-                      @if (settingsForm.get('confirmNewPassword')?.invalid &&
-                      settingsForm.get('confirmNewPassword')?.touched) {
-                      <label class="label">
-                        <span class="label-text-alt text-error"
-                          >Please confirm your new password</span
-                        >
-                      </label>
-                      } @if (fieldErrors()?.['confirmNewPassword']) {
-                      <label class="label">
-                        <span
-                          class="label-text-alt text-error"
-                          >{{ fieldErrors()?.['confirmNewPassword'] }}</span
-                        >
-                      </label>
-                      }
-                    </div>
-
-                    <div class="flex justify-end">
-                      <button
-                        type="submit"
-                        class="btn btn-primary"
-                        [disabled]="isLoading()"
-                      >
-                        @if (isLoading()) {
-                        <span class="loading loading-spinner loading-sm"></span>
-                        } Change Password
-                      </button>
-                    </div>
-                  </form>
-                </fieldset>
+      <app-header headerName="Settings" />
+      <!-- name of each tab group should be unique -->
+      <div class="tabs tabs-border">
+        <input
+          type="radio"
+          name="my_tabs_2"
+          class="tab"
+          aria-label="Profile"
+          checked="checked"
+        />
+        <div class="tab-content bg-base-100 p-2">
+          <fieldset class="fieldset">
+            <legend class="fieldset-legend text-base md:text-lg">
+              User Information
+            </legend>
+            <form [formGroup]="settingsForm" (ngSubmit)="updateUserInfo()">
+              <div class="form-control w-full mb-4">
+                <label class="label">
+                  <span class="label-text">Full Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your full name"
+                  class="input input-bordered w-full"
+                  formControlName="name"
+                />
+                @if (
+                  settingsForm.get('name')?.invalid &&
+                  settingsForm.get('name')?.touched
+                ) {
+                  <label class="label">
+                    <span class="label-text-alt text-error"
+                      >Please enter a valid name</span
+                    >
+                  </label>
+                }
               </div>
-            </div>
-          </section>
+
+              <div class="form-control w-full mb-6">
+                <label class="label">
+                  <span class="label-text">Email Address</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Enter your email address"
+                  class="input input-bordered w-full"
+                  formControlName="email"
+                />
+                @if (
+                  settingsForm.get('email')?.invalid &&
+                  settingsForm.get('email')?.touched
+                ) {
+                  <label class="label">
+                    <span class="label-text-alt text-error"
+                      >Please enter a valid email address</span
+                    >
+                  </label>
+                }
+                @if (fieldErrors()?.['email']) {
+                  <label class="label">
+                    <span class="label-text-alt text-error">{{
+                      fieldErrors()?.['email']
+                    }}</span>
+                  </label>
+                }
+              </div>
+
+              <div class="flex justify-end">
+                <button
+                  type="submit"
+                  class="btn btn-primary"
+                  [disabled]="settingsForm.invalid || isLoading()"
+                >
+                  @if (isLoading()) {
+                    <span class="loading loading-spinner loading-sm"></span>
+                  }
+                  Save Changes
+                </button>
+              </div>
+            </form>
+          </fieldset>
         </div>
 
+        <input
+          type="radio"
+          name="my_tabs_2"
+          class="tab"
+          aria-label="Authentication"
+        />
+        <div class="tab-content bg-base-100 p-2">
+          <!-- Change Password -->
+          <fieldset class="fieldset">
+            <legend class="fieldset-legend text-base md:text-lg">
+              Change Password
+            </legend>
+            <form [formGroup]="settingsForm" (ngSubmit)="changePassword()">
+              <div class="form-control w-full mb-4">
+                <label class="label">
+                  <span class="label-text">Current Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter your current password"
+                  class="input input-bordered w-full"
+                  formControlName="currentPassword"
+                />
+                @if (
+                  settingsForm.get('currentPassword')?.invalid &&
+                  settingsForm.get('currentPassword')?.touched
+                ) {
+                  <label class="label">
+                    <span class="label-text-alt text-error"
+                      >Please enter your current password</span
+                    >
+                  </label>
+                }
+              </div>
+
+              <div class="form-control w-full mb-4">
+                <label class="label">
+                  <span class="label-text">New Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter a new password"
+                  class="input input-bordered w-full"
+                  formControlName="newPassword"
+                />
+                @if (
+                  settingsForm.get('newPassword')?.invalid &&
+                  settingsForm.get('newPassword')?.touched
+                ) {
+                  <label class="label">
+                    <span class="label-text-alt text-error"
+                      >Please enter a new password</span
+                    >
+                  </label>
+                }
+              </div>
+
+              <div class="form-control w-full mb-6">
+                <label class="label">
+                  <span class="label-text">Confirm New Password</span>
+                </label>
+                <input
+                  type="password"
+                  class="input input-bordered w-full"
+                  placeholder="Confirm your new password"
+                  formControlName="confirmNewPassword"
+                />
+                @if (
+                  settingsForm.get('confirmNewPassword')?.invalid &&
+                  settingsForm.get('confirmNewPassword')?.touched
+                ) {
+                  <label class="label">
+                    <span class="label-text-alt text-error"
+                      >Please confirm your new password</span
+                    >
+                  </label>
+                }
+                @if (fieldErrors()?.['confirmNewPassword']) {
+                  <label class="label">
+                    <span class="label-text-alt text-error">{{
+                      fieldErrors()?.['confirmNewPassword']
+                    }}</span>
+                  </label>
+                }
+              </div>
+
+              <div class="flex justify-end">
+                <button
+                  type="submit"
+                  class="btn btn-primary"
+                  [disabled]="isLoading()"
+                >
+                  @if (isLoading()) {
+                    <span class="loading loading-spinner loading-sm"></span>
+                  }
+                  Change Password
+                </button>
+              </div>
+            </form>
+          </fieldset>
+        </div>
+      </div>
+      <div class="flex w-full flex-col">
         <div class="divider">OR</div>
 
         <div class="card rounded-box grid h-20 place-items-center">
@@ -239,7 +255,7 @@ export class SettingsComponent implements OnInit {
       error: () => {
         this.toast.error('Failed to load user data. Please try again later.');
         this.errorMessage.set(
-          'Failed to load user data. Please try again later.'
+          'Failed to load user data. Please try again later.',
         );
         this.isLoading.set(false);
       },
@@ -271,7 +287,7 @@ export class SettingsComponent implements OnInit {
       error: (err) => {
         this.isLoading.set(false);
         this.toast.error(
-          err.error?.message || 'Failed to update profile. Please try again.'
+          err.error?.message || 'Failed to update profile. Please try again.',
         );
         this.errorMessage.set(err.message || 'Update failed');
       },
@@ -319,15 +335,15 @@ export class SettingsComponent implements OnInit {
           this.isLoading.set(false);
           if (error.error?.errors) {
             this.toast.error(
-              error.error?.errors[0]?.message || 'Failed to change password.'
+              error.error?.errors[0]?.message || 'Failed to change password.',
             );
             this.handleErrors(error.error);
           } else {
             this.toast.error(
-              error.error?.message || 'Failed to change password.'
+              error.error?.message || 'Failed to change password.',
             );
             this.errorMessage.set(
-              error.error?.message || 'Failed to change password.'
+              error.error?.message || 'Failed to change password.',
             );
           }
         },
